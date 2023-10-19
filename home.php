80ce9b90a55f8ac973a0f6dd47a5bb8ba1ddc7b0
Tejas466 <121109169+Tejas466@users.noreply.github.com>
2023-10-20 02:41:38 +0530

    Update home.php

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     </head>
 <body>
 
